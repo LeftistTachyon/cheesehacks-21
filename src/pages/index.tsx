182733765225c179c330fe2c89c226ede393e2ae
LeftistTachyon <jedandeganwang1999@gmail.com>
2021-10-23 15:34:30 -0500

    Still trying to make it compile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { Box, Heading } from "@chakra-ui/layout";
 
 export default function Homepage(): JSX.Element {
-  return (
-    <Box>
-      <Heading as="h1">Welcome to the homepage!</Heading>
-      <Heading as="h3">We're still thinking of what to do...</Heading>
-    </Box>
-  );
+	return (
+		<Box>
+			<Heading as="h1">Welcome to the homepage!</Heading>
+			<Heading as="h3">We're still thinking of what to do...</Heading>
+		</Box>
+	);
 }
