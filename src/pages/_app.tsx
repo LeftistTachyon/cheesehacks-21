2e0e8a52225ec6f8bc391f2985d2d38e1a62209c
LeftistTachyon <jedandeganwang1999@gmail.com>
2021-10-23 16:35:06 -0500

    Adding blank header and footers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import theme from "@styles/theme";
 import Head from "next/head";
+import Header from "@components/header";
+import Footer from "@components/footer";
 
 export default function TheApp({
 	Component,
@@ -14,7 +16,9 @@ export default function TheApp({
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<ChakraProvider theme={theme}>
+				<Header />
 				<Component {...pageProps} />
+				<Footer />
 			</ChakraProvider>
 		</>
 	);
