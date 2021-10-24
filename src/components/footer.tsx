03c9348d9ab3002e5e3c6e928ce3bce39e4071f4
LeftistTachyon <jedandeganwang1999@gmail.com>
2021-10-23 20:20:43 -0500

    Make it build on Vercel

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,37 +1,26 @@
-import { ReactNode } from 'react';
-import {
-  Box,
-  Container,
-  Stack,
-  SimpleGrid,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { ReactNode } from "react";
+import { Box, Container, Stack, SimpleGrid, Text } from "@chakra-ui/react";
 
 const ListHeader = ({ children }: { children: ReactNode }) => {
-  return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
-      {children}
-    </Text>
-  );
+	return (
+		<Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+			{children}
+		</Text>
+	);
 };
 
-
 export default function LargeWithAppLinksAndSocial() {
-  return (
-    <Box
-      bgColor="brand.secondary"
-      color="brand.tertiary">
-      <Container as={Stack} maxW={'6xl'} py={10}>
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
-          <Stack align={'flex-start'} >
-            <ListHeader>Contact Us</ListHeader>
-            <Text>test@example.com</Text>
-			<Text>7894456325</Text>
-          </Stack>
-
-        </SimpleGrid>
-      </Container>
-    </Box>
-  );
+	return (
+		<Box bgColor="brand.secondary" color="brand.tertiary">
+			<Container as={Stack} maxW={"6xl"} py={10}>
+				<SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+					<Stack align={"flex-start"}>
+						<ListHeader>Contact Us</ListHeader>
+						<Text>test@example.com</Text>
+						<Text>(789) 445-6325</Text>
+					</Stack>
+				</SimpleGrid>
+			</Container>
+		</Box>
+	);
 }
