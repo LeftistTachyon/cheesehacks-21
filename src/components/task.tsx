7525aaf0a25dbd6b4fe60bbcaab7bad2c270113f
LeftistTachyon <jedandeganwang1999@gmail.com>
2021-10-24 02:50:59 -0500

    I might call it a night here w

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -4,7 +4,14 @@ import {
 	Button,
 	Box,
 	useDisclosure,
+	Text,
+	Flex,
 	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	NumberInputField,
+	NumberInput,
+	Input,
 } from "@chakra-ui/react";
 import {
 	Popover,
@@ -12,8 +19,9 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@chakra-ui/popover";
-import { Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import { Task } from "api/db";
+import React from "react";
 
 type TaskCardProps = {
 	task: Task;
@@ -22,6 +30,16 @@ type TaskCardProps = {
 
 export default function TaskCard(props: TaskCardProps): JSX.Element {
 	const { onOpen, onClose, isOpen } = useDisclosure();
+
+	function validateName(value: string): string {
+		if (!value) return "Name is required";
+	}
+	function validateDuration(hours: number, minutes: number) {
+		if (hours == 0 && minutes == 0)
+			return "Please enter a duration greater than zero";
+	}
+
+	const duration = props.task.duration;
 
 	return (
 		<Popover
@@ -43,38 +61,117 @@ export default function TaskCard(props: TaskCardProps): JSX.Element {
 			<PopoverContent p={5}>
 				<PopoverArrow />
 				<PopoverCloseButton />
-				{Form(onClose)}
+				<Formik
+					initialValues={{
+						name: props.task.name,
+						hours: duration.hours,
+						minutes: duration.minutes,
+					}}
+					onSubmit={(values, actions) => {
+						setTimeout(() => {
+							alert(JSON.stringify(values, null, 2));
+							onClose();
+							actions.setSubmitting(false);
+						}, 1000);
+					}}
+				>
+					{(props) => (
+						<Form>
+							<FormLabel>Duration</FormLabel>
+							<Flex justify="space-between" alignItems="center">
+								<Field
+									name="hours"
+									validate={(hours) =>
+										validateDuration(
+											hours,
+											props.values.minutes
+										)
+									}
+								>
+									{({ field, form }) => (
+										<FormControl
+											id="hours"
+											isInvalid={
+												form.errors.hours &&
+												form.touched.hours
+											}
+											isRequired
+										>
+											<NumberInput max={24} min={0}>
+												<NumberInputField
+													{...field}
+													placeholder="00"
+												/>
+											</NumberInput>
+										</FormControl>
+									)}
+								</Field>
+								<Text mx={3}>:</Text>
+								<Field name="minutes">
+									{({ field, form }) => (
+										<FormControl
+											id="minutes"
+											isInvalid={
+												form.errors.minutes &&
+												form.touched.minutes
+											}
+											isRequired
+										>
+											<NumberInput max={59} min={0}>
+												<NumberInputField
+													{...field}
+													placeholder="10"
+												/>
+											</NumberInput>
+										</FormControl>
+									)}
+								</Field>
+							</Flex>
+
+							<Field name="name" validate={validateName}>
+								{({ field, form }) => (
+									<FormControl
+										isInvalid={
+											form.errors.name &&
+											form.touched.name
+										}
+										isRequired
+										mt={4}
+									>
+										<FormLabel htmlFor="name">
+											Task name
+										</FormLabel>
+										<Input
+											{...field}
+											id="name"
+											placeholder="Task name"
+										/>
+										<FormErrorMessage>
+											{form.errors.name ||
+												form.errors.hours ||
+												form.errors.minutes}
+										</FormErrorMessage>
+									</FormControl>
+								)}
+							</Field>
+
+							<Box mt={4}>
+								<Button variant="outline" onClick={onClose}>
+									Cancel
+								</Button>
+								<Button
+									colorScheme="teal"
+									isLoading={props.isSubmitting}
+									type="submit"
+									ml={2}
+								>
+									Submit
+								</Button>
+							</Box>
+						</Form>
+					)}
+				</Formik>
 			</PopoverContent>
 		</Popover>
-	);
-}
-function Form(onClose: () => void): JSX.Element {
-	return (
-		<Formik
-			initialValues={{}}
-			onSubmit={(values, actions) => {
-				setTimeout(() => {
-					alert(JSON.stringify(values, null, 2));
-					actions.setSubmitting(false);
-				}, 1000);
-			}}
-		>
-			{(props) => (
-				<FormControl>
-					<Box mt={4}>
-						<Button variant="outline" onClick={onClose}>
-							Cancel
-						</Button>
-						<Button
-							colorScheme="teal"
-							isLoading={props.isSubmitting}
-							type="submit"
-						>
-							Submit
-						</Button>
-					</Box>
-				</FormControl>
-			)}
-		</Formik>
 	);
 }
