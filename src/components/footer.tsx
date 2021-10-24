5a9794fc7618378246b3c40357ca1280436b8553
ananyaativ <ananyaativ@gmail.com>
2021-10-23 19:04:48 -0500

    Update footer.tsx
    added footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,37 @@
-export default function Footer(): JSX.Element {
-	return <></>;
+import { ReactNode } from 'react';
+import {
+  Box,
+  Container,
+  Stack,
+  SimpleGrid,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+
+export default function LargeWithAppLinksAndSocial() {
+  return (
+    <Box
+      bgColor="brand.secondary"
+      color="brand.tertiary">
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+          <Stack align={'flex-start'} >
+            <ListHeader>Contact Us</ListHeader>
+            <Text>test@example.com</Text>
+			<Text>7894456325</Text>
+          </Stack>
+
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
